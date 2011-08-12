80c66c1bbeb5f103bf5f52378d0a37fbd51c9f9b
Derek Chafin <infomaniac50@gmail.com>
2011-08-12 17:06:19 -0500

    Inital Commit

---
+++ FilezillaXMLSiteGrabber/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FilezillaXMLSiteGrabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clstSites = new System.Windows.Forms.CheckedListBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.diaOpenXml = new System.Windows.Forms.OpenFileDialog();
+            this.diaSaveXml = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(378, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "&Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // clstSites
+            // 
+            this.clstSites.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clstSites.FormattingEnabled = true;
+            this.clstSites.IntegralHeight = false;
+            this.clstSites.Location = new System.Drawing.Point(12, 27);
+            this.clstSites.Name = "clstSites";
+            this.clstSites.Size = new System.Drawing.Size(354, 261);
+            this.clstSites.TabIndex = 1;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(291, 294);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // diaOpenXml
+            // 
+            this.diaOpenXml.DefaultExt = "xml";
+            this.diaOpenXml.FileName = "Filezilla";
+            this.diaOpenXml.Filter = "Filezilla XML|*.xml";
+            this.diaOpenXml.Title = "Select Filezilla XML File";
+            // 
+            // diaSaveXml
+            // 
+            this.diaSaveXml.DefaultExt = "xml";
+            this.diaSaveXml.FileName = "Filezilla";
+            this.diaSaveXml.Filter = "Filezilla XML|*.xml";
+            this.diaSaveXml.Title = "Save Grabbed Sites";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 329);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.clstSites);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Filezilla Site Grabber";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckedListBox clstSites;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.OpenFileDialog diaOpenXml;
+        private System.Windows.Forms.SaveFileDialog diaSaveXml;
+    }
+}
+
---
+++ FilezillaXMLSiteGrabber/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FilezillaXMLSiteGrabber
+{
+    public partial class Form1 : Form
+    {
+        private string filepath;
+        private const string root_element = "FileZilla3";
+        private const string servers_element = "Servers";
+        private const string server_element = "Server";
+        private const string name_element = "Name";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (diaOpenXml.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filepath = diaOpenXml.FileName;
+                LoadSites();
+            }
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExportSites();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            ExportSites();
+        }
+
+        void ExportSites()
+        {
+            if (!System.IO.File.Exists(filepath))
+            {
+                MessageBox.Show("File does not exist anymore!\r\nPlease select a new file.");
+                return;
+            }
+
+            XElement root_old = XElement.Load(filepath);
+
+            if (!ValidateXmlFile(root_old))
+            {
+                MessageBox.Show("The XML file selected does not appear to be a valid Filezilla XML file.\r\nIt might be that the file does not have any site manager entries.");
+                return;
+            }
+
+            if (diaSaveXml.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                XElement servers_new = new XElement(servers_element);
+                XElement root_new = new XElement(root_element,
+                    servers_new);
+
+                foreach (string item in clstSites.CheckedItems)
+                {
+                    XElement server = (from server_old in root_old.Descendants(server_element)
+                                       where server_old.Element(name_element).Value == item
+                                       select server_old).FirstOrDefault();
+
+                    servers_new.Add(server);
+                }
+
+                root_new.Save(diaSaveXml.FileName);
+            }
+        }
+
+
+
+        void LoadSites()
+        {
+            XElement root = XElement.Load(filepath);
+
+            if (!ValidateXmlFile(root))
+            {
+                MessageBox.Show("The XML file selected does not appear to be a valid Filezilla XML file.\r\nIt might be that the file does not have any site manager entries.");
+                return;
+            }
+
+            IEnumerable<XElement> servers = root.Descendants(server_element);
+
+            string[] server_names = (from server in servers
+                                     select server.Element(name_element).Value).ToArray();
+
+            clstSites.Items.AddRange(server_names);
+        }
+
+        bool ValidateXmlFile(XElement root)
+        {
+            if (root.Name != root_element)
+                return false;
+
+            if (root.Element(servers_element) == null)
+                return false;
+
+            if (root.Element(servers_element).Element(server_element) == null)
+                return false;
+
+            return true;
+        }
+    }
+
+
+}
